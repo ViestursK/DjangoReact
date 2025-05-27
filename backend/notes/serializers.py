bd71983e28739e0ad3a52a0a89c11479f8590114
ViestursK <95314459+ViestursK@users.noreply.github.com>
2025-05-27 19:39:23 +0300

    first commit

--- backend/notes/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-from .models import Note
-
-class NoteSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Note
-    fields = '__all__'
--- backend/notes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from .models import Note
-
-class NoteModelTest(TestCase):
-  def test_create_note(self):
-    note = Note.objects.create(title="Test Note #1", content="This is the content of the Test note #1.")
-    self.assertEqual(str(note), "Test Note #1")
