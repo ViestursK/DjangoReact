69c5c3afb79a974472f5b613f66df10617656185
ViestursK <95314459+ViestursK@users.noreply.github.com>
2025-05-27 18:20:47 +0300

    initial commit

---
+++ backend/notes/serializers.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Note
+    fields = '__all__'
